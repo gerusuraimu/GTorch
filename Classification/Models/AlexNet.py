a6b4e23faf554992df19ea1f1c486416c847a266
gerusuraimu <tohkams001@gmail.com>
2024-10-12 09:44:43 +0900

    Init

--- Classification/Models/AlexNet.py
+++ Classification/Models/AlexNet.py
@@ -8,18 +8,15 @@ from torchvision.models import AlexNet_Weights as Weights
 
 def alexnet(weights: bool = False, progress: bool = True) -> AlexNet:
     weights: Optional[Weights] = Weights.DEFAULT if weights else None
-    model = network(weights=weights, progress=progress)
+    model: AlexNet = network(weights=weights, progress=progress)
     return model
 
 
 def custom(model: AlexNet, config):
     features: int = model.classifier[-1].in_features
-    model.classifier[-1] = Linear(features, config.classes)
+    model.classifier[-1]: Linear = Linear(features, config.classes)
 
+    """
     if config.custom_in_gray:
         model.features[0] = Conv2d(1, 64, kernel_size=(11, 11), stride=(4, 4), padding=(2, 2))
-
-
-mod = alexnet(progress=True)
-mod.features[0] = Conv2d(1, 64, kernel_size=(11, 11), stride=(4, 4), padding=(2, 2))
-print(mod)
+    """
